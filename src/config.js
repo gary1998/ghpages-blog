7b036c8dac06fe28fa61ad8c144fb7778d449891
Gaurav Goswami <gouravgoswami48@gmail.com>
2020-11-06 20:20:20 +0530

    gary-blog

---
+++ src/config.js
@@ -0,0 +1,26 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "ZmI5ODljYmY3ZDA5NWU2MGY5ZDFlYjA2MDY5ODVkOGNlOGUxYjdkMQ==",
+
+  // Your Github UserName
+  githubUserName: "gary1998",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "react-blog-github",
+
+  // Your Personal Blog Title
+  title : "Gaurav Goswami" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "Software Developer @ IBM ISL Bangalore",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    backgroundColorDark: '#4C566A',
+    titleColor: '#ff5252',
+    titleColorDark: '#16a085',
+    subtitleColor: '#37474f',
+    subtitleColorDark: '#D8DEE9',
+  },
+};
