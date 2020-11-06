21c9049e6bd35aea93465933c62d7458c7294dac
Gaurav Goswami <gouravgoswami48@gmail.com>
2020-11-06 20:24:08 +0530

    Updates

---
+++ precache-manifest.6a52d76b058ee960b9aeae3a809417e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10716504ce118e826071c40650c02021",
+    "url": "/react-blog-github/index.html"
+  },
+  {
+    "revision": "a53f217deefccba4a87c",
+    "url": "/react-blog-github/static/css/main.cfa08aa7.chunk.css"
+  },
+  {
+    "revision": "cf352fcb46f8dc475556",
+    "url": "/react-blog-github/static/js/2.77de8f68.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/react-blog-github/static/js/2.77de8f68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a53f217deefccba4a87c",
+    "url": "/react-blog-github/static/js/main.9bf09f03.chunk.js"
+  },
+  {
+    "revision": "8216af6db86cb58257df",
+    "url": "/react-blog-github/static/js/runtime-main.b7e97252.js"
+  }
+]);
